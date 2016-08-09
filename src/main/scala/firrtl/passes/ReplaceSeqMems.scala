fb71b88ddad00df29a4d930b036c7f133f652853
Angie <shunshou@gmail.com>
2016-08-09 09:31:49 -0700

    conf generation works

--- src/main/scala/firrtl/passes/ReplaceSeqMems.scala
+++ src/main/scala/firrtl/passes/ReplaceSeqMems.scala
@@ -47,14 +47,16 @@ object ReplaceSeqMems extends Pass {
       moduleNamespace.newName(n + "_ext")
     }  
 
-    println("UNIQUE MEMORY MACROS NEEDED")
+    val conf = new java.io.BufferedWriter(new java.io.FileWriter("memory.conf"))
+    //println("UNIQUE MEMORY MACROS NEEDED")
     uniqueMems foreach { x => 
-      println(x.m.serialize)
+      conf write x.serialize
     }  
+    conf.close()
 
     val out = Circuit(c.info, updatedModules, c.main)
     //println(out.serialize)
-    println(out)
+    //println(out)
     out
   }
 
@@ -67,10 +69,30 @@ object ReplaceSeqMems extends Pass {
     numReaders: Int,
     numWriters: Int,
     numRWriters: Int,
+    noMask: Boolean,
+    mask_gran: Int,
+    total_width: Int,
     m: DefMemory
     // TODO: read under write
-  )
-  def create_proto(m: DefMemory) = {
+  ){
+    def serialize() = {
+      val writers = List.fill(numWriters)(if (noMask) "write" else "mwrite")
+      val readers = List.fill(numReaders)("read")
+      val readwriters = List.fill(numRWriters)(if (noMask) "rw" else "mrw")
+      val ports = (writers ++ readers ++ readwriters).mkString(",")
+      "name " + name + "\t" + 
+      "depth " + depth + 
+      " width " + total_width + 
+      " ports " + ports + 
+      ( if (!noMask) " mask_gran " + mask_gran else "" ) + 
+      "\n"
+      //name DataArray_DCache_Data_Array  depth 256 width 128 ports mwrite,read mask_gran 64
+      //name ICache_ICache_Data_Array_1  depth 128 width 128 ports rw
+      //name ICache_ICache_Tag_Array_1  depth 64 width 42 ports mrw mask_gran 21
+    }
+  }
+  def create_proto(m: DefMemory, noMask: Boolean) = {
+    // TODO: Add mask to spec?
     uniqueMems += MemProto(
       m.name + "_ext",
       m.dataType.serialize,
@@ -80,6 +102,9 @@ object ReplaceSeqMems extends Pass {
       m.readers.length,
       m.writers.length,
       m.readwriters.length,
+      noMask,
+      baseWidth(m.dataType).intValue,
+      totalWidth(m.dataType).intValue,
       m
     )
   }
@@ -122,7 +147,14 @@ object ReplaceSeqMems extends Pass {
           Seq(WSubField(ref,name,newType,gender(e)))
         else Seq(e)
       }
-      case t => Seq(e)
+      case t: UIntType => {
+        // TODO: Is this comprehensive? If mask isn't Vec, then don't need mask?
+        val ref = e.asInstanceOf[WSubField].exp
+        val name = e.asInstanceOf[WSubField].name
+        if (name == "mask") Seq(WSubField(ref,"NOTUSED",t,gender(e)))
+        else Seq(e)
+      }
+      case t => Seq(e) 
     }
   }
 
@@ -134,6 +166,16 @@ object ReplaceSeqMems extends Pass {
       case t: SIntType => t.width.asInstanceOf[IntWidth].width
       case t: UIntType => t.width.asInstanceOf[IntWidth].width
       case t: VectorType => baseWidth(t.tpe)
+      case t => -1
+    }
+  }
+
+  // TODO: Remove redundancy
+  def totalWidth(dataType: Type): BigInt = {
+    dataType match {
+      case t: SIntType => t.width.asInstanceOf[IntWidth].width
+      case t: UIntType => t.width.asInstanceOf[IntWidth].width
+      case t: VectorType => baseWidth(t.tpe)*t.size
       case t => -1
     }
   }
@@ -237,11 +279,17 @@ object ReplaceSeqMems extends Pass {
         // TODO: Anything else that can be a Vec? besides data/mask
       }
       case t => {
-        val extLoc = WSubField(extRef,lowerExtName(intPorts),t,swap(intGender))
-        memModStmts += {
-          if (intGender == FEMALE) Connect(NoInfo,intPorts,extLoc)
-          else Connect(NoInfo,extLoc,intPorts)
-        }
+        val portName = lowerExtName(intPorts)
+        // TODO: Don't use array buffer; use EmptyExpression or similar
+        // Don't connect write mask if its a UInt (would be same as WE)
+        val ignoreMask = lowerExtName(intPorts).split("_").last == "mask"
+        if (!ignoreMask){
+          val extLoc = WSubField(extRef,portName,t,swap(intGender))
+          memModStmts += {
+            if (intGender == FEMALE) Connect(NoInfo,intPorts,extLoc)
+            else Connect(NoInfo,extLoc,intPorts)
+          }
+        }  
       }
 
     }
@@ -255,10 +303,24 @@ object ReplaceSeqMems extends Pass {
 
     val dataBlockWidth = baseWidth(m.dataType)
 
+    // Unique # for each port
+
+    val oldMemPortNames = (m.writers ++ m.readers ++ m.readwriters)
+    val memPortNames = oldMemPortNames.zipWithIndex.map{case (x,i) => {
+      val identifier = {
+        if (m.readers.contains(x)) "R"
+        else if (m.writers.contains(x)) "W"
+        else "RW"
+      }
+      (x,identifier+i)
+    }}.toMap
+
+    /*
     val readers = m.readers.zipWithIndex.map{case (r,i) => (r,"R"+i)}
     val writers = m.writers.zipWithIndex.map{case (w,i) => (w,"W"+i)}
     val readwriters = m.readwriters.zipWithIndex.map{case (rw,i) => (rw,"RW"+i)}
     val memPortNames = (readers ++ writers ++ readwriters).toMap
+    */
 
     val memModStmts = ArrayBuffer[Statement]()
 
@@ -273,10 +335,19 @@ object ReplaceSeqMems extends Pass {
       exps map ( e => Port(m.info, LowerTypes.loweredName(e), to_dir(gender(e)), Utils.tpe(e)) )
     }*/
     
-    val blackBoxPorts = tpe.fields flatMap { f =>
+    val blackBoxPortsTemp = tpe.fields flatMap { f =>
       val exps = flattenMemVec(dataBlockWidth,WRef(memPortNames(f.name), f.tpe, PortKind(), toGender(f.flip)))
-      exps map ( e => Port(m.info, LowerTypes.loweredName(e), to_dir(gender(e)), Utils.tpe(e)) )
+      exps map ( e => {
+        val portName = LowerTypes.loweredName(e)
+        //val isMask = portName.split("_").last == "mask"
+        Port(m.info, portName, to_dir(gender(e)), Utils.tpe(e)) 
+      })
     }
+
+    val blackBoxPorts = blackBoxPortsTemp.toSeq.filter(x => x.name.split("_").last != "NOTUSED")
+    //println(blackBoxPorts)
+    val noMask = ((blackBoxPortsTemp.length-1) == blackBoxPorts.length)
+
 
     val blackBoxName = m.name + "_ext"
     val memBlackBox = ExtModule(m.info,blackBoxName,blackBoxPorts)
@@ -285,7 +356,7 @@ object ReplaceSeqMems extends Pass {
 
    if (uniqueMems.isEmpty){
       // Is this uniquely identifiable
-      create_proto(m)
+      create_proto(m,noMask)
       newModules += memBlackBox
       memModStmts += WDefInstance(m.info,blackBoxName,blackBoxName,blackBoxType)
     }
@@ -302,7 +373,7 @@ object ReplaceSeqMems extends Pass {
       })
 
       if (duplicate == None){
-        create_proto(m)
+        create_proto(m,noMask)
         newModules += memBlackBox
         memModStmts += WDefInstance(m.info,blackBoxName,blackBoxName,blackBoxType)
       }
@@ -347,29 +418,10 @@ object ReplaceSeqMems extends Pass {
 
 }
 
+// check that mask is consistent with order of read/write vec
 // TODO: Support multiple latencies?
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // TODO: Mem tiling pass?
-// If wmask = true or wmode
+// run inferreadwrite here
 
 //////////////////////////////////////////////////////////////
 // Extra reset
-// mask gran??
-// no mask
-// conf-like?
-// match name
